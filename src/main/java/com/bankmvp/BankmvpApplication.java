d1b27e901a26799288f92a035b0201f3fb0d5b83
Nikhil Kishore <nuth.nik@gmail.com>
2025-08-29 22:24:03 +0530

    chore: initial project setup (STS + local Postgres)

---
+++ src/main/java/com/bankmvp/BankmvpApplication.java
@@ -0,0 +1,13 @@
+package com.bankmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankmvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankmvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankmvp/BankmvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankmvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
